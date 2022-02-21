097c75068bbb03f9f79d82022eaa5c2ced86e8fd
Renato Napoli <renato.napoli@nagro.app>
2022-02-21 11:25:46 -0300

    Feat: Login / Cadastro de usuario

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button type="submit" class="btn btn-primary brn-block">Logar</button>
+      <router-link :to="{ name: 'novo.usuario' }">
+        Não possui um cadastro, cadastre-se aqui!
+      </router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      usuario: {},
+    };
+  },
+};
+</script>
--- src/views/NovoUsuario.vue
+++ src/views/NovoUsuario.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>Novo usuário.</h1>
+  <div class="container">
+    <form @submit.prevent="enviarFormulario">
+      <h1>Novo Usuário</h1>
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" v-model="usuario.nome" />
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NovoUsuario",
+  data() {
+    return {
+      usuario: {
+        nome: "",
+        senha: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    enviarFormulario() {
+      axios
+        .post("http://localhost:8000/auth/register", this.usuario)
+        .then((resposta) => {
+          console.log(resposta);
+          this.$router.push({ name: "login" });
+        })
+
+        .catch((erro) => console.log(erro));
+    },
+  },
+};
+</script>
