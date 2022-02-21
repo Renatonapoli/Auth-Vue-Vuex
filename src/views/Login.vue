0f0cbc3fafd8bc9e77c8f33cc6f5aea0b026a016
Renato Napoli <renato.napoli@nagro.app>
2022-02-21 11:58:54 -0300

    Feat: Acao de login e logout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1>Login</h1>
-    <form>
+    <form @submit.prevent="efetuarLogin">
       <div class="form-group">
         <label for="email">Email</label>
         <input type="email" class="form-control" v-model="usuario.email" />
@@ -19,12 +19,26 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Login",
   data() {
     return {
       usuario: {},
     };
+  },
+  methods: {
+    efetuarLogin() {
+      axios
+        .post("http://localhost:8000/auth/login", this.usuario)
+        .then((response) => {
+          console.log(response);
+          localStorage.setItem("token", response.data.access_token);
+          this.$router.push({ name: "gerentes" });
+        })
+        .catch((erro) => console.log(erro));
+    },
   },
 };
 </script>
