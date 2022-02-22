331f835d3575bf7cc5ce951ad7b6e984d8393ace
Renato Napoli <renato.napoli@nagro.app>
2022-02-21 21:09:49 -0300

    Feat: Login / Logout

---
+++ frontend/src/components/BarraNavegacao.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-bytebank">
+    <a class="navbar-brand" href="#">ByteBank</a>
+    <button class="navbar-toggler" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <BarraNavegacaoQuandoLogado v-if="usuarioEstaLogado" />
+      <BarraNavegacaoQuandoDeslogado v-else />
+    </div>
+  </nav>
+</template>
+
+<script>
+import BarraNavegacaoQuandoLogado from "./BarraNavegacaoQuandoLogado";
+import BarraNavegacaoQuandoDeslogado from "./BarraNavegacaoQuandoDeslogado";
+export default {
+  name: "BarraNavegacao",
+  components: { BarraNavegacaoQuandoLogado, BarraNavegacaoQuandoDeslogado },
+  computed: {
+    usuarioEstaLogado() {
+      return Boolean(localStorage.getItem("token"));
+    },
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
